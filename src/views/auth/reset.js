ef21b1f918dac61c43b8d04f4f8a31fe305ddaf7
hou27 <ataj125@gmail.com>
2022-07-26 18:10:24 +0900

    Add Reset component

--- src/views/auth/reset.js
+++ src/views/auth/reset.js
@@ -6,7 +6,13 @@ import { instance } from "../../lib/interceptors";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../../localKey";
 import { getCookie, setCookie } from "../../utils/cookie";
 
-export default function Login({ userId }) {
+const FormError = ({ errorMessage }) => (
+  <span role="alert" className="font-medium text-red-500">
+    {errorMessage}
+  </span>
+);
+
+export default function Reset() {
   const history = useHistory();
 
   const {
@@ -19,9 +25,9 @@ export default function Login({ userId }) {
   });
 
   async function onSubmit() {
-    const { email, password } = getValues();
+    const { email } = getValues();
     await instance
-      .post("api/auth/login", { email, password })
+      .post("api/auth/send-password-reset-email", { email })
       .then(function (res) {
         console.log(res);
         const { access_token, refresh_token } = res.data;
@@ -52,9 +58,17 @@ export default function Login({ userId }) {
         <div className="flex content-center items-center justify-center h-full">
           <div className="w-full lg:w-4/12 px-4">
             <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
+              <div className="rounded-t mb-0 px-6 py-6">
+                <div className="text-center mb-3">
+                  <h6 className="text-blueGray-500 text-sm font-bold">
+                    Enter the mail to find the password
+                  </h6>
+                </div>
+                <hr className="mt-6 border-b-1 border-blueGray-300" />
+              </div>
               <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
                 <div className="text-blueGray-400 text-center mb-3 font-bold">
-                  <small>Or sign in with credentials</small>
+                  <small>Enter your Email</small>
                 </div>
                 <form onSubmit={handleSubmit(onSubmit)}>
                   <div className="relative w-full mb-3">
@@ -73,36 +87,9 @@ export default function Login({ userId }) {
                       className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                       placeholder="Email"
                     />
-                  </div>
-
-                  <div className="relative w-full mb-3">
-                    <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                      htmlFor="grid-password"
-                    >
-                      Password
-                    </label>
-                    <input
-                      {...register("password", {
-                        required: "Password is required",
-                      })}
-                      required
-                      type="password"
-                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      placeholder="Password"
-                    />
-                  </div>
-                  <div>
-                    <label className="inline-flex items-center cursor-pointer">
-                      <input
-                        id="customCheckLogin"
-                        type="checkbox"
-                        className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
-                      />
-                      <span className="ml-2 text-sm font-semibold text-blueGray-600">
-                        Remember me
-                      </span>
-                    </label>
+                    {errors.name?.message && (
+                      <FormError errorMessage={errors.name?.message} />
+                    )}
                   </div>
 
                   <div className="text-center mt-6">
@@ -110,18 +97,14 @@ export default function Login({ userId }) {
                       className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
                       type="submit"
                     >
-                      Sign In
+                      Confirm
                     </button>
                   </div>
                 </form>
               </div>
             </div>
             <div className="flex flex-wrap mt-6 relative">
-              <div className="w-1/2">
-                <Link to="/auth/reset" className="text-blueGray-200">
-                  <small>Forgot password?</small>
-                </Link>
-              </div>
+              <div className="w-1/2 text-left"></div>
               <div className="w-1/2 text-right">
                 <Link to="/auth/register" className="text-blueGray-200">
                   <small>Create new account</small>
